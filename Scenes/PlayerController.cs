fdeb570715d7fbe20c34ded2c7cccf3b0bf38412
MChandra111 <hashmahesh@gmail.com>
2023-11-13 18:53:21 -0600

    Main

--- Scenes/PlayerController.cs
+++ Scenes/PlayerController.cs
@@ -1,8 +1,20 @@
 using Godot;
 using System;
+//using System.Numerics;
 
 public partial class PlayerController : CharacterBody2D
 {
+	public enum PlayerState
+	{
+		Idle,
+		Running,
+		Attacking,
+		Dashing,
+		TakingDamage,
+		Dead,
+		WallJump
+	}
+	public PlayerState CurrentState = PlayerState.Idle;
 	private const float Speed = 200.0f;
 	private const float JumpVelocity = -400.0f;
 	private const float DoubleJumpVelocity = -300.0f;
@@ -12,7 +24,7 @@ public partial class PlayerController : CharacterBody2D
 	private bool isDashing = false;
 	private float dashTimer = .2f;
 	private float dashTimerReset = .2f;
-	private bool isDashAvailable = false;
+	private bool isDashAvailable = true;
 	private bool isWallJumping = false;
 	private float wallJumpTimer = .45f;
 	private float wallJumpTimerReset = .45f;
@@ -26,106 +38,111 @@ public partial class PlayerController : CharacterBody2D
 	[Signal]
 	public delegate void DeathEventHandler();
 
-    public override void _Ready()
-    {
-        Anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
-    }
-
-    public override void _PhysicsProcess(double delta)
+	public override void _Ready()
 	{
-		Vector2 velocity = Velocity;
+		Anim = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		velocity = Velocity;
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "Jump", "ui_down");
 
-		if (Health > 0){
-			if (!IsOnFloor()){
-				velocity.Y += gravity * (float)delta;
-			}
+		inputManager(direction, delta);
+		switch (CurrentState)
+		{
+			case PlayerState.Idle:
+				idle(direction, delta);
+				break;
+			case PlayerState.Attacking:
+				break;
+			case PlayerState.Running:
+				processHorizontalMovement(direction, delta);
+				break;
+			case PlayerState.Dashing:
+				processDash(delta);
+				break;
+			case PlayerState.TakingDamage:
+				processTakingDamage();
+				break;
+			case PlayerState.Dead:
+				velocity = new Vector2(0, 0);
+				break;
+		}
 
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+
+	private void inputManager(Vector2 direction, double delta)
+	{
+		if (!IsOnFloor()){
+			velocity.Y += gravity * (float)delta;
+		}
+
+		if (isTakingDamage){
+			CurrentState = PlayerState.TakingDamage;
+		}
+
+		if (Health <= 0){
+			CurrentState = PlayerState.Dead;
+		}
+
+		if (Health > 0 && !isTakingDamage){
 			// Handle Jump
-			if (JumpCount < JumpMax){
-				if (Input.IsActionJustPressed("Jump")){
+			if (JumpCount < JumpMax)
+			{
+				if (Input.IsActionJustPressed("Jump"))
+				{
 					velocity.Y = JumpVelocity;
 					JumpCount += 1;
-
 					//Double Jump
-					if (Input.IsActionJustPressed("Jump")  && !IsOnFloor() && JumpCount >= 1){
+					if (Input.IsActionJustPressed("Jump") && !IsOnFloor() && JumpCount >= 1)
+					{
 						velocity.Y = DoubleJumpVelocity;
 					}
 				}
 			}
-			
 
 			//Resetting jump count
-			if(IsOnFloor() && JumpCount != 0){
+			if (IsOnFloor() && JumpCount != 0)
+			{
 				JumpCount = 0;
 			}
 
-			//Dash Reset
-			if(IsOnFloor()){
-				isDashAvailable = true;
+			if (Input.IsActionPressed("Attack"))
+			{
+				CurrentState = PlayerState.Attacking;
 			}
-
-			//WallJumping
-			if (Input.IsActionJustPressed("Jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding() && !IsOnFloor()){
-				velocity.Y = JumpVelocity;
-				velocity.X = -JumpVelocity/2;
-				isWallJumping = true;
-				Anim.FlipH = false;
-				Anim.Play("Jump");
-				JumpCount = 0;
-			} else if (Input.IsActionJustPressed("Jump") && GetNode<RayCast2D>("RayCastRight").IsColliding() && !IsOnFloor()){
-				velocity.Y = JumpVelocity;
-				velocity.X = JumpVelocity/2;
-				isWallJumping = true;
-				Anim.FlipH = true;
-				Anim.Play("Jump");
-				JumpCount = 0;
-			}
-
-			if (isWallJumping){
-				wallJumpTimer -= (float)delta;
-				if (wallJumpTimer <= 0){
-					isWallJumping = false;
-					wallJumpTimer = wallJumpTimerReset;
+			if (Input.IsActionPressed("ui_right"))
+			{
+				if (!isWallJumping){
+					Anim.FlipH = false;
 				}
+				Anim.Play("Run");
+				facingDirection = 1;
+				CurrentState = PlayerState.Running;
 			}
-
-			// Horizontal movement handling
-			//Movement only allowed if not dashing
-			if (!isDashing && !isWallJumping && !isTakingDamage){
-				if (direction != Vector2.Zero)
-				{
-						velocity.X = direction.X * Speed;
-						if (Input.IsActionPressed("ui_right")){
-							Anim.FlipH = false;
-							if (Input.IsActionJustPressed("Dash")){
-								Anim.Play("Dash");
-							}
-							Anim.Play("Run");
-							facingDirection = 1;
-						}
-						if (Input.IsActionPressed("ui_left")){
-							Anim.FlipH = true;
-							Anim.Play("Run");
-							facingDirection = -1;
-						}
+			if (Input.IsActionPressed("ui_left"))
+			{
+				if (!isWallJumping){
+					Anim.FlipH = true;
 				}
-				else
-				{
-					if(IsOnFloor()){
-						Anim.Play("Idle");
-					}
-					velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-				}
+				Anim.Play("Run");
+				facingDirection = -1;
+				CurrentState = PlayerState.Running;
+			}
+			if (Input.IsActionPressed("Dash"))
+			{
+				CurrentState = PlayerState.Dashing;
 			}
 
+			processWallJump(delta);
 
-			if((isTakingDamage && velocity.Y == 0) || Input.IsActionJustPressed("Jump") || Input.IsActionJustPressed("Dash")){
-				isTakingDamage = false;
+			if (direction == Vector2.Zero){
+				CurrentState = PlayerState.Idle;
 			}
 
-			//Logic for falling/jumping animations (comes after horizontal movement, 
-			// since vertical animations look better in the air while falling)
 			if (!isTakingDamage){
 				if (velocity.Y > 0){
 					Anim.Play("Fall");
@@ -133,51 +150,122 @@ public partial class PlayerController : CharacterBody2D
 				if (velocity.Y < 0 && JumpCount == 0){
 					Anim.Play("Jump");
 				}
-				if (velocity.Y < 0 && JumpCount == 1 && !GetNode<RayCast2D>("RayCastLeft").IsColliding() && !GetNode<RayCast2D>("RayCastRight").IsColliding()){
+				if (velocity.Y < 0 && JumpCount == 1){
 					Anim.Play("DoubleJump");
 				}
 			}
 
-			//Dashing
-			if (isDashAvailable){
-				if (Input.IsActionJustPressed("Dash")){
-					if (Input.IsActionPressed("ui_right")){
-						velocity.X = dashSpeed;
-						isDashing = true;
-					}
-					if (Input.IsActionPressed("ui_left")){
-						velocity.X = -dashSpeed;
-						isDashing = true;
-					}
-
-					dashTimer = dashTimerReset;
-					isDashAvailable = false;
-				}
+			if (IsOnFloor() && !isDashing){
+				isDashAvailable = true;
 			}
 
-			if(isDashing){
+			if (isDashing)
+			{
 				dashTimer -= (float)delta;
-				if(dashTimer <= 0){
+				if (dashTimer <= 0)
+				{
 					isDashing = false;
 					velocity = new Vector2(0, 0);
 				}
 			}
+		}
+	}
 
-			Velocity = velocity;
-			MoveAndSlide();
+	private void idle(Vector2 direction, double delta)
+	{
+		if (direction == Vector2.Zero)
+		{
+			if (IsOnFloor())
+			{
+				Anim.Play("Idle");
+			}
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+	}
+
+	private void processHorizontalMovement(Vector2 direction, double delta)
+	{
+		if (!isDashing && !isWallJumping && !isTakingDamage && direction != Vector2.Zero)
+		{
+			velocity.X = direction.X * Speed;
+		}
+	}
+
+	private void processWallJump(double delta)
+	{
+		//WallJumping
+		if (Input.IsActionJustPressed("Jump") && GetNode<RayCast2D>("RayCastLeft").IsColliding() && !IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+			velocity.X = -JumpVelocity / 2;
+			isWallJumping = true;
+			Anim.FlipH = false;
+			Anim.Play("Jump");
+			JumpCount = 0;
+		}
+		else if (Input.IsActionJustPressed("Jump") && GetNode<RayCast2D>("RayCastRight").IsColliding() && !IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+			velocity.X = JumpVelocity / 2;
+			isWallJumping = true;
+			Anim.FlipH = true;
+			Anim.Play("Jump");
+			JumpCount = 0;
+		}
+
+		if (isWallJumping)
+		{
+			wallJumpTimer -= (float)delta;
+			if (wallJumpTimer <= 0)
+			{
+				isWallJumping = false;
+				wallJumpTimer = wallJumpTimerReset;
+			}
+		}
+	}
+
+	private void processDash(double delta)
+	{
+		//Dashing
+		if (isDashAvailable)
+		{
+			if (Input.IsActionPressed("ui_right"))
+			{
+				velocity.X = dashSpeed;
+				isDashing = true;
+			}
+			if (Input.IsActionPressed("ui_left"))
+			{
+				velocity.X = -dashSpeed;
+				isDashing = true;
+			}
+
+			dashTimer = dashTimerReset;
+			isDashAvailable = false;
+		}
+	}
+
+	private void processTakingDamage()
+	{
+		if ((isTakingDamage && Velocity.Y == 0) || Input.IsActionJustPressed("Jump") || Input.IsActionJustPressed("Dash"))
+		{
+			isTakingDamage = false;
 		}
 	}
 
 	//Taking Damage code
-	public void TakeDamage(){
-		if (Health > 0){
+	public void TakeDamage()
+	{
+		if (Health > 0)
+		{
 			Health -= 1;
 			GD.Print(Health);
 			Velocity = new Vector2(100f * -facingDirection, -300);
 			MoveAndSlide();
 			isTakingDamage = true;
 			Anim.Play("TakeDamage");
-			if (Health <= 0){
+			if (Health <= 0)
+			{
 				Health = 0;
 				Anim.Play("Death");
 			}
@@ -185,8 +273,10 @@ public partial class PlayerController : CharacterBody2D
 	}
 
 	//Stopping death animation
-	private void _on_animated_sprite_2d_animation_finished(){
-		if(Anim.Animation == "Death"){
+	private void _on_animated_sprite_2d_animation_finished()
+	{
+		if (Anim.Animation == "Death")
+		{
 			Anim.Stop();
 			Hide();
 			EmitSignal(SignalName.Death);
@@ -194,7 +284,8 @@ public partial class PlayerController : CharacterBody2D
 	}
 
 	//Respawning player code
-	public void RespawnPlayer(){
+	public void RespawnPlayer()
+	{
 		Show();
 		Health = 3;
 		Velocity = new Vector2(0, 0);
